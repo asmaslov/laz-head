509cf582181014f3f78a8d96cb22bff681d635e0
maslov <none@none>
2016-09-07 12:42:50 +0400

    Test new commands support

--- serial_test.py
+++ serial_test.py
@@ -31,6 +31,34 @@ class MainWindow(QtGui.QMainWindow):
             self.ui.menuPort.addAction(node)
         self.tim.timeout.connect(self.readPort)
 
+    def keyPressEvent(self, event):
+        if type(event) == QtGui.QKeyEvent and not event.isAutoRepeat():
+            if event.key() == QtCore.Qt.Key_W:
+                self.on_pushButtonUp_pressed()
+            elif event.key() == QtCore.Qt.Key_S:
+                self.on_pushButtonDown_pressed()
+            elif event.key() == QtCore.Qt.Key_A:
+                self.on_pushButtonLeft_pressed()
+            elif event.key() == QtCore.Qt.Key_D:
+                self.on_pushButtonRight_pressed()
+            event.accept()
+        else:
+            event.ignore()
+
+    def keyReleaseEvent(self, event):
+        if type(event) == QtGui.QKeyEvent and not event.isAutoRepeat():
+            if event.key() == QtCore.Qt.Key_W:
+                self.on_pushButtonUp_released()
+            elif event.key() == QtCore.Qt.Key_S:
+                self.on_pushButtonDown_released()
+            elif event.key() == QtCore.Qt.Key_A:
+                self.on_pushButtonLeft_released()
+            elif event.key() == QtCore.Qt.Key_D:
+                self.on_pushButtonRight_released()
+            event.accept()
+        else:
+            event.ignore()
+
     def closeEvent(self, event):
         choice = QtGui.QMessageBox.question(self, 'Exit', 'Are you sure?', QtGui.QMessageBox.Yes| QtGui.QMessageBox.No)
         if choice == QtGui.QMessageBox.Yes:
@@ -39,65 +67,175 @@ class MainWindow(QtGui.QMainWindow):
             event.accept()
         else:
             event.ignore()
-            
-    def on_pushButtonMove_released(self):
+
+    def sendPacket(self, data):
         if self.ser.isOpen():
             packet = bytearray()
             crc = 0
             packet.append(0x21)
-            packet.append(0x01)
-            if isint(self.ui.lineEditRotAngle.text()):
-                rotAngle = int(self.ui.lineEditRotAngle.text())
-                packet.append(((abs(rotAngle) >> 8) & 0xFF) | ((1 << 7) if (rotAngle < 0) else 0))
-                packet.append(abs(rotAngle) & 0xFF)
-            else:
-                packet.append(0x00)
-                packet.append(0x00)
-            if isint(self.ui.lineEditTiltAngle.text()):
-                tiltAngle = int(self.ui.lineEditTiltAngle.text())
-                packet.append(((abs(tiltAngle) >> 8) & 0xFF) | ((1 << 7) if (tiltAngle < 0) else 0))
-                packet.append(abs(tiltAngle) & 0xFF)
-            else:
-                packet.append(0x00)
-                packet.append(0x00)
-            if isint(self.ui.lineEditRotSpeed.text()):
-                packet.append(abs(int(self.ui.lineEditRotSpeed.text())) & 0xFF)
-            else:
-                packet.append(0x00)
-            if isint(self.ui.lineEditTiltSpeed.text()):    
-                packet.append(abs(int(self.ui.lineEditTiltSpeed.text())) & 0xFF)
-            else:
-                packet.append(0x00)
+            for i in range (0, 7):
+                if (len(data) > i):
+                    packet.append(data[i])
+                else:
+                    packet.append(0x00)
             for one in packet:
                 crc = crc + one
             packet.append((crc >> 8) & 0xFF)
             packet.append(crc & 0xFF)
             #print ' '.join('0x{:02X}'.format(x) for x in packet)
             self.ser.write(packet)
-            self.ui.lineEditRotAngle.clear()
-            self.ui.lineEditTiltAngle.clear()
-            self.ui.lineEditRotSpeed.clear()
-            self.ui.lineEditTiltSpeed.clear()
         else:
             self.ui.statusbar.showMessage('Port not open')
+            
+    def on_pushButtonMove_released(self):
+        packet = bytearray()
+        packet.append(0x01)
+        if isint(self.ui.lineEditRotAngle.text()):
+            rotAngle = int(self.ui.lineEditRotAngle.text())
+            packet.append(((abs(rotAngle) >> 8) & 0xFF) | ((1 << 7) if (rotAngle < 0) else 0))
+            packet.append(abs(rotAngle) & 0xFF)
+        else:
+            packet.append(0x00)
+            packet.append(0x00)
+        if isint(self.ui.lineEditTiltAngle.text()):
+            tiltAngle = int(self.ui.lineEditTiltAngle.text())
+            packet.append(((abs(tiltAngle) >> 8) & 0xFF) | ((1 << 7) if (tiltAngle < 0) else 0))
+            packet.append(abs(tiltAngle) & 0xFF)
+        else:
+            packet.append(0x00)
+            packet.append(0x00)
+        if isint(self.ui.lineEditRotSpeed.text()):
+            packet.append(abs(int(self.ui.lineEditRotSpeed.text())) & 0xFF)
+        else:
+            packet.append(0x00)
+        if isint(self.ui.lineEditTiltSpeed.text()):    
+            packet.append(abs(int(self.ui.lineEditTiltSpeed.text())) & 0xFF)
+        else:
+            packet.append(0x00)
+        self.sendPacket(packet)
+        self.ui.lineEditRotAngle.clear()
+        self.ui.lineEditTiltAngle.clear()
+        self.ui.lineEditRotSpeed.clear()
+        self.ui.lineEditTiltSpeed.clear()
 
     def on_pushButtonRead_released(self):
-        if self.ser.isOpen():
-            packet = bytearray()
-            packet.append(0x21)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x00)
-            packet.append(0x80)
-            #print ' '.join('0x{:02X}'.format(x) for x in packet)
-            self.ser.write(packet)
+        packet = bytearray()
+        packet.append(0x00)
+        self.sendPacket(packet)
+
+    def on_pushButtonLeft_pressed(self):
+        packet = bytearray()
+        packet.append(0x03)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        if isint(self.ui.lineEditRotSpeed.text()):
+            packet.append(abs(int(self.ui.lineEditRotSpeed.text())) | 0x80)
         else:
-            self.ui.statusbar.showMessage('Port not open')
+            packet.append(0x00)
+        self.sendPacket(packet)
+        
+    def on_pushButtonLeft_released(self):
+        packet = bytearray()
+        packet.append(0x02)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        self.sendPacket(packet)
+        
+    def on_pushButtonRight_pressed(self):
+        packet = bytearray()
+        packet.append(0x03)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        if isint(self.ui.lineEditRotSpeed.text()):
+            packet.append(abs(int(self.ui.lineEditRotSpeed.text())) & 0x7F)
+        else:
+            packet.append(0x00)
+        self.sendPacket(packet)
+            
+    def on_pushButtonRight_released(self):
+        packet = bytearray()
+        packet.append(0x02)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        self.sendPacket(packet)
+
+    def on_pushButtonDown_pressed(self):
+        packet = bytearray()
+        packet.append(0x03)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x01)
+        if isint(self.ui.lineEditRotSpeed.text()):
+            packet.append(abs(int(self.ui.lineEditTiltSpeed.text())) | 0x80)
+        else:
+            packet.append(0x00)
+        self.sendPacket(packet)
+
+    def on_pushButtonDown_released(self):
+        packet = bytearray()
+        packet.append(0x02)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x01)
+        self.sendPacket(packet)
+
+    def on_pushButtonUp_pressed(self):
+        packet = bytearray()
+        packet.append(0x03)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x01)
+        if isint(self.ui.lineEditRotSpeed.text()):
+            packet.append(abs(int(self.ui.lineEditTiltSpeed.text())) & 0x7F)
+        else:
+            packet.append(0x00)
+        self.sendPacket(packet)
+
+    def on_pushButtonUp_released(self):
+        packet = bytearray()
+        packet.append(0x02)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x01)
+        self.sendPacket(packet)
+
+    def on_pushButtonStop_released(self):
+        packet = bytearray()
+        packet.append(0x02)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        self.sendPacket(packet)
+        packet = bytearray()
+        packet.append(0x02)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x01)
+        self.sendPacket(packet)
     
     @QtCore.pyqtSlot(bool)
     def on_actionOpen_triggered(self, arg):
@@ -145,17 +283,17 @@ class MainWindow(QtGui.QMainWindow):
             data = map(ord, self.ser.read(10))
             for i in range (0, len(data) - 2):
                 crc = crc + data[i]
-            if (crc == ((data[8] << 8) | data[9])):
+            if crc == ((data[8] << 8) | data[9]):
                 #print ' '.join('0x{:02X}'.format(x) for x in data)
                 if (data[0] == 0xA1):
-                    if (data[1] == 0xFF):
+                    if data[1] == 0xFF:
                         self.ui.statusbar.showMessage('Acknowledged')
-                    elif (data[1] == 0xFE):
+                    elif data[1] == 0xFE:
                         rotAngle = int(((data[2] & 0x7F) << 8) | data[3])
-                        if ((data[2] & 0x80) != 0):
+                        if (data[2] & 0x80) != 0:
                             rotAngle = -rotAngle
                         tiltAngle = int(((data[4] & 0x7F) << 8) | data[5])
-                        if ((data[4] & 0x80) != 0):
+                        if (data[4] & 0x80) != 0:
                             tiltAngle = -tiltAngle
                         self.ui.lineEditRotAngle.setText(str(rotAngle))
                         self.ui.lineEditTiltAngle.setText(str(tiltAngle))
